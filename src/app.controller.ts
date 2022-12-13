94950ccedc5d1ba304e7626e3c64915f9c6d82e2
Gabriel Rocha <rochafrgabriel@gmail.com>
2022-12-13 10:13:33 -0300

    add post method, validating the body from request

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,8 +1,9 @@
 import { randomUUID } from 'node:crypto';
-import { Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 
 import { MailService } from './mail/mail.service';
 import { PrismaService } from './prisma.service';
+import { CreateNotificationBody } from './create-notification-body';
 
 @Controller('notifications')
 export class AppController {
@@ -14,13 +15,15 @@ export class AppController {
   }
 
   @Post()
-  async create() {
+  async create(@Body() body: CreateNotificationBody) {
+    const { recipientId, content, category } = body;
+
     await this.prisma.notification.create({
       data: {
         id: randomUUID(),
-        content: 'New friend solicitation',
-        category: 'social',
-        recipientId: randomUUID(),
+        content,
+        category,
+        recipientId,
       },
     });
   }
