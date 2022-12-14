d14c2e0eb0d8536baa6eda40d685ded529562097
Gabriel Rocha <rochafrgabriel@gmail.com>
2022-12-14 09:30:26 -0300

    create in memory database and repositories for unit test, redo the db connection

---
+++ src/infra/http/controller/notifications.controller.ts
@@ -0,0 +1,23 @@
+import { randomUUID } from 'node:crypto';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+
+import { CreateNotificationBody } from '../dtos/create-notification-body';
+import { SendNotification } from 'src/app/use-cases/send-notification';
+
+@Controller('notifications')
+export class NotificationsController {
+  constructor(private sendNotification: SendNotification) {}
+
+  @Post()
+  async create(@Body() body: CreateNotificationBody) {
+    const { recipientId, content, category } = body;
+
+    const { notification } = await this.sendNotification.execute({
+      recipientId,
+      content,
+      category,
+    });
+
+    return { notification };
+  }
+}
