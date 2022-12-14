15df791081426a90cc14e8165b441510b8f7b9f2
Gabriel Rocha <rochafrgabriel@gmail.com>
2022-12-14 09:02:54 -0300

    content tests

--- src/app/entities/content.spec.ts
+++ src/app/entities/content.spec.ts
@@ -1,15 +1,17 @@
 import { Content } from './content';
 
-test('should be able to create a notifcation content', () => {
-  const content = new Content('Friend request');
+describe('Notification Content Testes', () => {
+  it('should be able to create a notifcation content', () => {
+    const content = new Content('Friend request');
 
-  expect(content.value).toBeTruthy();
-});
+    expect(content.value).toBeTruthy();
+  });
 
-test('should not be able to create a notifcation content with less than 5 chars', () => {
-  expect(() => new Content('Frie')).toThrow();
-});
+  it('should not be able to create a notifcation content with less than 5 chars', () => {
+    expect(() => new Content('Frie')).toThrow();
+  });
 
-test('should not be able to create a notifcation content with more than 240 chars', () => {
-  expect(() => new Content('a'.repeat(241))).toThrow();
+  it('should not be able to create a notifcation content with more than 240 chars', () => {
+    expect(() => new Content('a'.repeat(241))).toThrow();
+  });
 });
